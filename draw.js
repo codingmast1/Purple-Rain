f23ac35befabe0afa118cf90eac3d9d6b6763fe2
Shivam Tahalani <47814999+codingmast1@users.noreply.github.com>
2019-02-20 16:09:28 +0530

    Add files via upload

---
+++ draw.js
@@ -0,0 +1,16 @@
+var drops = [];
+
+function setup() {
+  createCanvas(640, 600);
+  for (var i = 0; i < 500; i++) {
+    drops[i] = new Drop();
+  }
+}
+
+function draw() {
+  background(230, 230, 250);
+  for (var i = 0; i < drops.length; i++) {
+    drops[i].fall();
+    drops[i].show();
+  }
+}
